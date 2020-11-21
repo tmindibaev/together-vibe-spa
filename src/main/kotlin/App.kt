9d02b371163a3294364ddf3e03b79d9bb7ec5956
Mindibaev Timofei <t.mindibaev@qiwi.com>
2020-11-21 23:47:18 +0300

    adjust button style

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,18 +1,28 @@
 import kotlinx.css.Align
 import kotlinx.css.Color
+import kotlinx.css.Cursor
 import kotlinx.css.Display
 import kotlinx.css.FlexDirection
 import kotlinx.css.LinearDimension
 import kotlinx.css.alignSelf
 import kotlinx.css.backgroundColor
 import kotlinx.css.borderRadius
+import kotlinx.css.borderWidth
+import kotlinx.css.cursor
 import kotlinx.css.display
 import kotlinx.css.flexDirection
 import kotlinx.css.fontSize
 import kotlinx.css.height
+import kotlinx.css.left
 import kotlinx.css.marginBottom
 import kotlinx.css.marginTop
+import kotlinx.css.minWidth
+import kotlinx.css.padding
+import kotlinx.css.paddingLeft
+import kotlinx.css.paddingRight
 import kotlinx.css.px
+import kotlinx.css.right
+import kotlinx.css.top
 import kotlinx.css.width
 import kotlinx.html.InputType
 import react.RBuilder
@@ -43,10 +53,10 @@ class App : RComponent<RProps, AppState>() {
             styledInput(type = InputType.text, name = "activityText") {
                 css {
                     backgroundColor = Color.whiteSmoke
-                    borderRadius = LinearDimension("7px")
+                    borderRadius = LinearDimension("15px")
                     fontSize = LinearDimension("20px")
                     height = LinearDimension("30px")
-                    width = LinearDimension("300px")
+                    width = LinearDimension("600px")
                     marginBottom = 20.px
                 }
             }
@@ -55,9 +65,13 @@ class App : RComponent<RProps, AppState>() {
                 css {
                     borderRadius = LinearDimension("7px")
                     backgroundColor = Color.lightGray
+                    borderWidth = LinearDimension("1px")
                     height = LinearDimension("30px")
-                    width = LinearDimension("100px")
+                    width = LinearDimension("150px")
                     fontSize = LinearDimension("16px")
+                    paddingLeft = LinearDimension("16px")
+                    paddingRight = LinearDimension("16px")
+                    cursor = Cursor.pointer
                 }
             }
         }
